2425710eb2c705d3fed769b025a4066309eb1802
XiaZhengXin <xzx@xiazhengxin.name>
2019-11-21 18:19:59 +0800

    衣柜审核 后台功能加入。。。。。。。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

---
+++ src/main/java/cn/offway/ares/controller/AuditController.java
@@ -0,0 +1,76 @@
+package cn.offway.ares.controller;
+
+import cn.offway.ares.domain.PhWardrobeAudit;
+import cn.offway.ares.service.PhWardrobeAuditService;
+import cn.offway.ares.service.PhWardrobeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class AuditController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private PhWardrobeAuditService wardrobeAuditService;
+    @Autowired
+    private PhWardrobeService wardrobeService;
+
+    @RequestMapping("/audit.html")
+    public String index(ModelMap map) {
+        return "audit_index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/audit_list")
+    public Map<String, Object> getStockList(int sEcho, int iDisplayStart, int iDisplayLength, String brandId, String goodsName, String goodsId, String state) {
+        Sort sort = new Sort("id");
+        PageRequest pr = new PageRequest(iDisplayStart == 0 ? 0 : iDisplayStart / iDisplayLength, iDisplayLength < 0 ? 9999999 : iDisplayLength, sort);
+        Page<PhWardrobeAudit> pages = wardrobeAuditService.listAll(brandId, goodsName, goodsId, state, pr);
+        int initEcho = sEcho + 1;
+        Map<String, Object> map = new HashMap<>();
+        map.put("sEcho", initEcho);
+        map.put("iTotalRecords", pages.getTotalElements());//数据总条数
+        map.put("iTotalDisplayRecords", pages.getTotalElements());//显示的条数
+        map.put("aData", pages.getContent());//数据集合
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("/audit_findOne")
+    public PhWardrobeAudit findOne(Long id) {
+        return wardrobeAuditService.findOne(id);
+    }
+
+    @ResponseBody
+    @RequestMapping("/audit_up")
+    public boolean allow(Long id) {
+        PhWardrobeAudit obj = wardrobeAuditService.findOne(id);
+        if (obj != null) {
+            obj.setState("1");
+            wardrobeAuditService.save(obj);
+        }
+        return true;
+    }
+
+    @ResponseBody
+    @RequestMapping("/audit_down")
+    public boolean deny(Long id, String str) {
+        PhWardrobeAudit obj = wardrobeAuditService.findOne(id);
+        if (obj != null) {
+            obj.setState("2");
+            obj.setReason(str);
+            wardrobeAuditService.save(obj);
+        }
+        return true;
+    }
+}
--- src/main/java/cn/offway/ares/service/PhWardrobeAuditService.java
+++ src/main/java/cn/offway/ares/service/PhWardrobeAuditService.java
@@ -1,9 +1,11 @@
 package cn.offway.ares.service;
 
 
-import java.util.List;
-
 import cn.offway.ares.domain.PhWardrobeAudit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 /**
  * 衣柜审核Service接口
@@ -11,13 +13,15 @@ import cn.offway.ares.domain.PhWardrobeAudit;
  * @author wn
  * @version $v: 1.0.0, $time:2019-11-21 17:04:14 Exp $
  */
-public interface PhWardrobeAuditService{
+public interface PhWardrobeAuditService {
 
     PhWardrobeAudit save(PhWardrobeAudit phWardrobeAudit);
-	
+
     PhWardrobeAudit findOne(Long id);
 
     void delete(Long id);
 
     List<PhWardrobeAudit> save(List<PhWardrobeAudit> entities);
+
+    Page<PhWardrobeAudit> listAll(String brandId, String goodsName, String goodsId, String state, Pageable pageable);
 }
