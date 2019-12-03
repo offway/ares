2effebda339cd9ccf40bcbd25bf8fa307d9436ef
XiaZhengXin <xzx@xiazhengxin.name>
2019-12-03 13:49:06 +0800

    用户验证审核通过 发送 订阅消息至微信。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/ares/service/impl/PhAuthServiceImpl.java
+++ src/main/java/cn/offway/ares/service/impl/PhAuthServiceImpl.java
@@ -1,26 +1,18 @@
 package cn.offway.ares.service.impl;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import cn.offway.ares.domain.PhAuth;
 import cn.offway.ares.domain.PhCode;
 import cn.offway.ares.domain.PhCreditDetail;
 import cn.offway.ares.domain.PhUserInfo;
 import cn.offway.ares.dto.Template;
-import cn.offway.ares.dto.TemplateParam;
 import cn.offway.ares.repository.PhAuthRepository;
 import cn.offway.ares.service.PhAuthService;
 import cn.offway.ares.service.PhCodeService;
 import cn.offway.ares.service.PhCreditDetailService;
 import cn.offway.ares.service.PhUserInfoService;
 import cn.offway.ares.utils.HttpClientUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,8 +23,11 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.*;
 
 
 /**
@@ -44,156 +39,187 @@ import com.alibaba.fastjson.JSONObject;
 @Service
 public class PhAuthServiceImpl implements PhAuthService {
 
-	private static Logger logger = LoggerFactory.getLogger(PhAuthServiceImpl.class);
+    private static Logger logger = LoggerFactory.getLogger(PhAuthServiceImpl.class);
 
-	@Autowired
-	private PhAuthRepository phAuthRepository;
-	
-	@Autowired
-	private PhUserInfoService phUserInfoService;
-	
-	@Autowired
-	private PhCreditDetailService phCreditDetailService;
-	
-	@Autowired
-	private PhCodeService phCodeService;
-	
-	@Override
-	public PhAuth save(PhAuth phAuth){
-		return phAuthRepository.save(phAuth);
-	}
-	
-	@Override
-	public PhAuth findOne(Long id){
-		return phAuthRepository.findOne(id);
-	}
-	
-	@Override
-	public Page<PhAuth> findByPage(final String status,final String nickName,final String unionid,Pageable page){
-		return phAuthRepository.findAll(new Specification<PhAuth>() {
-			
-			@Override
-			public Predicate toPredicate(Root<PhAuth> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-				List<Predicate> params = new ArrayList<Predicate>();
-				
-				if(StringUtils.isNotBlank(status)){
-					params.add(criteriaBuilder.equal(root.get("status"), status));
-				}
-				
-				if(StringUtils.isNotBlank(nickName)){
-					params.add(criteriaBuilder.like(root.get("nickname"), "%"+nickName+"%"));
-				}
-				
-				if(StringUtils.isNotBlank(unionid)){
-					params.add(criteriaBuilder.equal(root.get("unionid"), unionid));
-				}
-				
+    @Autowired
+    private PhAuthRepository phAuthRepository;
+
+    @Autowired
+    private PhUserInfoService phUserInfoService;
+
+    @Autowired
+    private PhCreditDetailService phCreditDetailService;
+
+    @Autowired
+    private PhCodeService phCodeService;
+
+    @Override
+    public PhAuth save(PhAuth phAuth) {
+        return phAuthRepository.save(phAuth);
+    }
+
+    @Override
+    public PhAuth findOne(Long id) {
+        return phAuthRepository.findOne(id);
+    }
+
+    @Override
+    public Page<PhAuth> findByPage(final String status, final String nickName, final String unionid, Pageable page) {
+        return phAuthRepository.findAll(new Specification<PhAuth>() {
+
+            @Override
+            public Predicate toPredicate(Root<PhAuth> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> params = new ArrayList<Predicate>();
+
+                if (StringUtils.isNotBlank(status)) {
+                    params.add(criteriaBuilder.equal(root.get("status"), status));
+                }
+
+                if (StringUtils.isNotBlank(nickName)) {
+                    params.add(criteriaBuilder.like(root.get("nickname"), "%" + nickName + "%"));
+                }
+
+                if (StringUtils.isNotBlank(unionid)) {
+                    params.add(criteriaBuilder.equal(root.get("unionid"), unionid));
+                }
+
                 Predicate[] predicates = new Predicate[params.size()];
                 criteriaQuery.where(params.toArray(predicates));
-				return null;
-			}
-		}, page);
-	}
-	
-	@Override
-	public boolean authUpdate(Long id,String status,String approvalContent,Authentication authentication){
-		PhAuth phAuth = findOne(id);
-		if(StringUtils.isNotBlank(approvalContent)){
-			phAuth.setApprovalContent(approvalContent);
-		}
-		phAuth.setStatus(status);
-		phAuth.setApproval(new Date());
-		phAuth.setApprover(authentication.getName());
-		save(phAuth);
-		
-		PhUserInfo phUserInfo = phUserInfoService.findByUnionid(phAuth.getUnionid());
-		
-		if("1".equals(status)){
-			phUserInfo.setIsAuth("1");
-			phUserInfo.setPhone(phAuth.getPhone());
-			phUserInfo.setPosition(phAuth.getPosition());
-			phUserInfo.setIdcardObverse(phAuth.getIdcardObverse());
-			phUserInfo.setIdcardPositive(phAuth.getIdcardPositive());
-			phUserInfo.setRealName(phAuth.getRealName());
-			phUserInfo.setInCert(phAuth.getInCert());
-			phUserInfo.setCompanyName(phAuth.getCompanyName());
-			phUserInfo.setCreditScore(50L);
-			phUserInfoService.save(phUserInfo);
-			
-			PhCreditDetail creditDetail = new PhCreditDetail();
-			creditDetail.setChannel("认证通过");
-			creditDetail.setCreateName(phAuth.getApprover());
-			creditDetail.setCreateTime(new Date());
-			creditDetail.setOrderNo(null);
-			creditDetail.setRemark(null);
-			creditDetail.setScore(50L);
-			creditDetail.setType("0");
-			creditDetail.setUnionid(phAuth.getUnionid());
-			phCreditDetailService.save(creditDetail);
-			
-		}
-		String openid = phUserInfo.getMiniopenid();
-		String formid = phAuth.getFormId();
+                return null;
+            }
+        }, page);
+    }
 
-		// 模块消息配置
-		Template tem = new Template();
-		tem.setTemplateId("3XfYDBQWMwRfEsvmRhemNtZVy-j5dFoNPXoCz7t4QwI");
-		tem.setFormId(formid);
-		tem.setTopColor("#00DD00");
-		tem.setToUser(openid);
-		tem.setPage("pages/index/index");
-		String result = "您的身份审核已通过";
-		String content = "恭喜！可以借衣啦！";
-		if ("2".equals(status)) {
-			result = "您的身份审核未通过";
-			content = approvalContent;
+    @Override
+    public boolean authUpdate(Long id, String status, String approvalContent, Authentication authentication) {
+        PhAuth phAuth = findOne(id);
+        if (StringUtils.isNotBlank(approvalContent)) {
+            phAuth.setApprovalContent(approvalContent);
+        }
+        phAuth.setStatus(status);
+        phAuth.setApproval(new Date());
+        phAuth.setApprover(authentication.getName());
+        save(phAuth);
 
-			PhCode phCode = phCodeService.findOne(phAuth.getCodeId());
-			phCode.setStatus("0");
-			phCodeService.save(phCode);
+        PhUserInfo phUserInfo = phUserInfoService.findByUnionid(phAuth.getUnionid());
 
-		}
-		List<TemplateParam> paras = new ArrayList<TemplateParam>();
-		paras.add(new TemplateParam("keyword1", result, "#0044BB"));
-		paras.add(new TemplateParam("keyword2", content, "#0044BB"));
-		//tem.setEmphasis_keyword("keyword1.DATA");
-		tem.setTemplateParamList(paras);
-		// 推送模版消息
-		sendTemplateMsg(tem, getToken());
-		return true;
-	}
+        if ("1".equals(status)) {
+            phUserInfo.setIsAuth("1");
+            phUserInfo.setPhone(phAuth.getPhone());
+            phUserInfo.setPosition(phAuth.getPosition());
+            phUserInfo.setIdcardObverse(phAuth.getIdcardObverse());
+            phUserInfo.setIdcardPositive(phAuth.getIdcardPositive());
+            phUserInfo.setRealName(phAuth.getRealName());
+            phUserInfo.setInCert(phAuth.getInCert());
+            phUserInfo.setCompanyName(phAuth.getCompanyName());
+            phUserInfo.setCreditScore(50L);
+            phUserInfoService.save(phUserInfo);
 
-	/**
-	 * 获取 access_token
-	 */
-	public static String getToken() {
-		String requestUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx8dac79ed90eb9311&secret=8e2d761b60cd91b7ca8d3883c378fa0d";
-		String result = HttpClientUtil.post(requestUrl, "");
-		JSONObject jsonObject = JSON.parseObject(result);
-		if (jsonObject != null) {
-			return jsonObject.getString("access_token");
-		} else {
-			return "";
-		}
-	}
+            PhCreditDetail creditDetail = new PhCreditDetail();
+            creditDetail.setChannel("认证通过");
+            creditDetail.setCreateName(phAuth.getApprover());
+            creditDetail.setCreateTime(new Date());
+            creditDetail.setOrderNo(null);
+            creditDetail.setRemark(null);
+            creditDetail.setScore(50L);
+            creditDetail.setType("0");
+            creditDetail.setUnionid(phAuth.getUnionid());
+            phCreditDetailService.save(creditDetail);
 
-	public static void sendTemplateMsg(Template template, String token) {
-		String requestUrl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=ACCESS_TOKEN";
-		requestUrl = requestUrl.replace("ACCESS_TOKEN", token);
-		String jsonString = template.toJSON();
+        }
+        String openid = phUserInfo.getMiniopenid();
+//        String formid = phAuth.getFormId();
 
-		String result = HttpClientUtil.post(requestUrl, jsonString);
-		JSONObject jsonResult = JSON.parseObject(result);
-		if (jsonResult != null) {
-			int errorCode = jsonResult.getIntValue("errcode");
-			String errorMessage = jsonResult.getString("errmsg");
-			if (errorCode == 0) {
-				logger.info("模板消息发送成功:" + jsonResult);
-			} else {
-				logger.info("模板消息发送失败:" + errorCode + "," + errorMessage);
-			}
-		} else {
-			logger.info("模板消息发送失败:" + jsonResult);
-		}
-	}
+        // 模块消息配置
+//        Template tem = new Template();
+//        tem.setTemplateId("3XfYDBQWMwRfEsvmRhemNtZVy-j5dFoNPXoCz7t4QwI");
+//        tem.setFormId(formid);
+//        tem.setTopColor("#00DD00");
+//        tem.setToUser(openid);
+//        tem.setPage("pages/index/index");
+//        List<TemplateParam> paras = new ArrayList<TemplateParam>();
+//        paras.add(new TemplateParam("keyword1", result, "#0044BB"));
+//        paras.add(new TemplateParam("keyword2", content, "#0044BB"));
+//        tem.setTemplateParamList(paras);
+//        // 推送模版消息
+//        sendTemplateMsg(tem, getToken());
+
+        //发送订阅消息
+        String result = "您的身份审核已通过";
+        String content = "恭喜！可以借衣啦！";
+        if ("2".equals(status)) {
+            result = "您的身份审核未通过";
+            content = approvalContent;
+            PhCode phCode = phCodeService.findOne(phAuth.getCodeId());
+            phCode.setStatus("0");
+            phCodeService.save(phCode);
+        }
+        Map<String, Object> args = new HashMap<>();
+        args.put("touser", openid);
+        args.put("template_id", "XXXXXXXXXXXXXXXXXX");
+        Map<String, Object> data = new HashMap<>();
+        Map<String, String> k1 = new HashMap<>();
+        k1.put("value", result);
+        data.put("key1", k1);
+        Map<String, String> k2 = new HashMap<>();
+        k1.put("value", content);
+        data.put("key2", k2);
+        args.put("data", data);
+        sendSubscribeMsg(args, getToken());
+        return true;
+    }
+
+    /**
+     * 获取 access_token
+     */
+    public static String getToken() {
+        String requestUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxd39e2b6036febef9&secret=4ceed4a1664ce0692565e6e3277f4a9b";
+        String result = HttpClientUtil.get(requestUrl);
+        JSONObject jsonObject = JSON.parseObject(result);
+        if (jsonObject != null) {
+            return jsonObject.getString("access_token");
+        } else {
+            return "";
+        }
+    }
+
+    public static void sendTemplateMsg(Template template, String token) {
+        String requestUrl = "https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=ACCESS_TOKEN";
+        requestUrl = requestUrl.replace("ACCESS_TOKEN", token);
+        String jsonString = template.toJSON();
+
+        String result = HttpClientUtil.post(requestUrl, jsonString);
+        JSONObject jsonResult = JSON.parseObject(result);
+        if (jsonResult != null) {
+            int errorCode = jsonResult.getIntValue("errcode");
+            String errorMessage = jsonResult.getString("errmsg");
+            if (errorCode == 0) {
+                logger.info("模板消息发送成功:" + jsonResult);
+            } else {
+                logger.info("模板消息发送失败:" + errorCode + "," + errorMessage);
+            }
+        } else {
+            logger.info("模板消息发送失败");
+        }
+    }
+
+    public static void sendSubscribeMsg(Map<String, Object> template, String token) {
+        String requestUrl = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token=ACCESS_TOKEN";
+        requestUrl = requestUrl.replace("ACCESS_TOKEN", token);
+        String jsonString = JSONObject.toJSONString(template);
+
+        String result = HttpClientUtil.post(requestUrl, jsonString);
+        JSONObject jsonResult = JSON.parseObject(result);
+        if (jsonResult != null) {
+            int errorCode = jsonResult.getIntValue("errcode");
+            String errorMessage = jsonResult.getString("errmsg");
+            if (errorCode == 0) {
+                logger.info("订阅消息发送成功:" + jsonResult);
+            } else {
+                logger.info("订阅消息发送失败:" + errorCode + "," + errorMessage);
+            }
+        } else {
+            logger.info("订阅消息发送失败");
+        }
+    }
 }
