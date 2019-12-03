a2c136c837b9975c31172f768884ced47bc14575
XiaZhengXin <xzx@xiazhengxin.name>
2019-12-03 11:27:28 +0800

    衣柜审核完毕 根据通过与否发送对应的模板消息。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/ares/controller/AuditController.java
+++ src/main/java/cn/offway/ares/controller/AuditController.java
@@ -1,11 +1,17 @@
 package cn.offway.ares.controller;
 
 import cn.offway.ares.domain.PhAdmin;
+import cn.offway.ares.domain.PhUserInfo;
 import cn.offway.ares.domain.PhWardrobe;
 import cn.offway.ares.domain.PhWardrobeAudit;
+import cn.offway.ares.dto.Template;
+import cn.offway.ares.dto.TemplateParam;
 import cn.offway.ares.service.PhBrandService;
+import cn.offway.ares.service.PhUserInfoService;
 import cn.offway.ares.service.PhWardrobeAuditService;
 import cn.offway.ares.service.PhWardrobeService;
+import cn.offway.ares.service.impl.PhAuthServiceImpl;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +39,8 @@ public class AuditController {
     private PhWardrobeService wardrobeService;
     @Autowired
     private PhBrandService brandService;
+    @Autowired
+    private PhUserInfoService userInfoService;
 
     @RequestMapping("/audit.html")
     public String index(ModelMap map, Authentication authentication) {
@@ -77,27 +86,57 @@ public class AuditController {
     @RequestMapping("/audit_up")
     public boolean allow(Long id) {
         PhWardrobeAudit obj = wardrobeAuditService.findOne(id);
-        PhWardrobe wardrobe = wardrobeService.findOne(obj.getWardrobeId());
         if (obj != null) {
+            PhWardrobe wardrobe = wardrobeService.findOne(obj.getWardrobeId());
             obj.setState("1");
             wardrobeAuditService.save(obj);
             wardrobe.setState("1");
             wardrobeService.save(wardrobe);
+            PhUserInfo userInfo = userInfoService.findByUnionid(obj.getUnionid());
+            if (userInfo != null) {
+                sendMsg(userInfo.getMiniopenid(), obj.getFormId(), null);
+            }
         }
         return true;
+    }
+
+    private void sendMsg(String openid, String formid, String approvalContent) {
+        // 模块消息配置
+        Template tem = new Template();
+        tem.setTemplateId("3XfYDBQWMwRfEsvmRhemNtZVy-j5dFoNPXoCz7t4QwI");
+        tem.setFormId(formid);
+        tem.setTopColor("#00DD00");
+        tem.setToUser(openid);
+        tem.setPage("pages/index/index");
+        String result = "审核通知";
+        String content = "你在OFFWAY MODE SHOWROOM申请的服装已通过审核，请及时提交订单借衣。";
+        if (StringUtils.isNotBlank(approvalContent)) {
+            result = " 审核失败";
+            content = "您在OFFWAY MODE SHOWROOM申请的借衣未通过审核，理由：" + approvalContent;
+        }
+        List<TemplateParam> paras = new ArrayList<TemplateParam>();
+        paras.add(new TemplateParam("keyword1", result, "#0044BB"));
+        paras.add(new TemplateParam("keyword2", content, "#0044BB"));
+        tem.setTemplateParamList(paras);
+        // 推送模版消息
+        PhAuthServiceImpl.sendTemplateMsg(tem, PhAuthServiceImpl.getToken());
     }
 
     @ResponseBody
     @RequestMapping("/audit_down")
     public boolean deny(Long id, String str) {
         PhWardrobeAudit obj = wardrobeAuditService.findOne(id);
-        PhWardrobe wardrobe = wardrobeService.findOne(obj.getWardrobeId());
         if (obj != null) {
+            PhWardrobe wardrobe = wardrobeService.findOne(obj.getWardrobeId());
             obj.setState("2");
             obj.setReason(str);
             wardrobeAuditService.save(obj);
             wardrobe.setState("2");
             wardrobeService.save(wardrobe);
+            PhUserInfo userInfo = userInfoService.findByUnionid(obj.getUnionid());
+            if (userInfo != null) {
+                sendMsg(userInfo.getMiniopenid(), obj.getFormId(), str);
+            }
         }
         return true;
     }
